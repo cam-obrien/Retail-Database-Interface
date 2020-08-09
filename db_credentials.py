150f49d7aca800ba766d7b6be4be25040a209c22
Jonesy22 <43551656+Jonesy22@users.noreply.github.com>
2020-08-09 12:57:53 -0700

    Add files via upload

---
+++ db_credentials.py
@@ -0,0 +1,4 @@
+host = 'classmysql.engr.oregonstate.edu'  #DON'T CHANGE ME UNLESS THE INSTRUCTIONS SAY SO
+user = 'cs340_jonesd5' #CHANGE ME
+passwd = '3756' #CHANGE ME
+db = 'cs340_jonesd5' #CHANGE ME
