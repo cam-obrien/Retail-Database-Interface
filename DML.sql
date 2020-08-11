6dcc67d9784508fb0829fab13c7ab38792f80e91
Daniel Jones <jonesd5@flip3.engr.oregonstate.edu>
2020-08-11 14:49:59 -0700

    Editing DML.sql

--- DML.sql
+++ DML.sql
@@ -1,58 +1,48 @@
 -- B) INSERT INTO queries to get ready to populate the tables
 ------------------------------------------------------------------------------------------------------------------------------------------------------
-INSERT INTO Customers (customerID, email, firstName, lastName, address, dob, phone, city, state, zipcode)
-VALUES (:customerIDInput, :emailInput, :firstNameInput, :lastNameInput, :addressInput, :dobInput, :phoneInput, :cityInput, :stateInput, :zipcodeInput);
+INSERT INTO Customers (email, firstName, lastName, address, dob, phone, city, state, zipcode) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s);
 
-INSERT INTO Orders (orderID, customerID, productID, dateOrdered, dateDelivered, totalPrice)
-VALUES (:orderIDInput, :customerIDInput, :productIDInput, :dateOrderedInput, :dateDeliveredInput, :totalPriceInput);
+INSERT INTO Orders (customerID, productID, dateOrdered, dateDelivered, totalPrice) VALUES (%s,%s,%s,%s,%s);
 
-INSERT INTO Products (productID, productName, brandName, price, category, sale, color)
-VALUES (:productInput, :productNameInput, :brandNameInput, :priceInput, :categoryInput, :saleInput, :colorInput);
+INSERT INTO Products (productName, brandName, price, category, sale, color) VALUES (%s,%s,%s,%s,%s,%s);
 
-INSERT INTO Stores (storeID, city, state, address, hours, daysOpen)
-VALUES (:storeIDInput, :cityInput, :stateInput, :addressInput, :hoursInput, :daysOpenInput);
+INSERT INTO Stores (address, city, state, daysOpen, hours) VALUES (%s,%s,%s,%s,%s);
 ------------------------------------------------------------------------------------------------------------------------------------------------------
 -- SELECT STATEMENTS to view all of the entries via View Orders/Customers/Stores/Products button
 -- For "View Orders" button
-SELECT orderID AS "Order ID", customerID AS "Customer ID", dateOrdered AS "Date Ordered", dateDelivered AS "Date Delivered", totalPrice AS "Total Price" FROM Orders;
--- For "View Products" button
-SELECT productID AS "Product ID", productName AS "Product Name", brandName AS "Brand Name", price AS "Price", category AS "Category", sale AS "On Sale", color AS "Color" FROM Products;
--- For "View Customers" button
-SELECT customerID AS "Customer ID", email AS "Email", firstName AS "First Name", lastName AS "Last Name", address AS "Address", dob AS "DOB", phone AS "Phone", city AS "City", state AS "State", zipcode AS "Zipcode" FROM Customers;
--- For "View Stores" button
-SELECT storeID AS "Store ID", city AS "City", state AS "State", address AS "Address", hours AS "Hours", daysOpen AS "Days Open" FROM Stores;
+SELECT orderID, customerID, productID, dateOrdered, dateDelivered, totalPrice FROM Orders;
+
+SELECT productID, productName, brandName, price, category, sale, color FROM Products;
+
+SELECT customerID, email, firstName, lastName, address, dob, phone, city, state, zipcode FROM Customers;
+
+SELECT storeID, address, city, state, daysOpen, hours FROM Stores;
 ------------------------------------------------------------------------------------------------------------------------------------------------------
 -- UPDATE Statements to update orders/customers/stores/prducts
 -- For edit Customers buttons
-UPDATE Customers;
-SET customerID = :customerIDInput, email = :emailInput, firstName = :firstNameInput, lastName = :lastNameInput, address = :addressInput, dob = :dobInput, phone = :phoneInput, city = :cityInput, state = :stateInput, zipcode = :zipcodeInput;
-WHERE customerID = :selectedCustomerID;
+UPDATE Customers SET email = %s, firstName = %s, lastName = %s, address = %s, dob = %s, phone = %s, city = %s, state = %s, zipcode = %s WHERE customerID = %s;
 
 -- For edit Orders buttons
-UPDATE Orders;
-SET orderID = :orderIDInput, customerID = :customerIDInput, productID = :productIDInput, dateOrdered = :dateOrderedInput, dateDelivered = :dateDeliveredInput, totalPrice = :totalPriceInput;
-WHERE orderID = :selectedOrderID;
+UPDATE Orders SET customerID = %s, productID = %s, dateOrdered = %s, dateDelivered = %s, totalPrice = %s WHERE orderID = %s;
 
 -- For edit Products buttons
-UPDATE Products;
-SET productID = :productIDInput, productName = :productNameInput, brandName = :brandNameInput, price = :priceInput, category = :categoryInput, sale = :saleInput, color = :colorInput;
-WHERE productID = :selectedProductID;
+UPDATE Products SET productName = %s, brandName = %s, price = %s, category = %s, sale = %s, color = %s WHERE productID = %s;
 
 -- For edit Stores buttons
-UPDATE Stores;
-SET storeID = :storeIDInput, city = :cityInput, state = :stateInput, address = :addressInput, hours = :hoursInput, daysOpen = :daysOpenInput;
-WHERE storeID = :selectedStoreID;
+UPDATE Stores SET address = %s, city = %s, state = %s, daysOpen = %s, hours = %s WHERE storeID = %s;
 
+--For edit Order_Products button
+UPDATE Order_Products SET productID = %s WHERE orderID = %s;
 ------------------------------------------------------------------------------------------------------------------------------------------------------
 -- DELETE Statements to update orders/customers/stores/prducts
 -- For delete Customers buttons
-DELETE FROM Customers WHERE customerID = :selectedCustomerID;
+DELETE FROM Customers WHERE customerID = %s;
 
 -- For delete Orders buttons
-DELETE FROM Orders WHERE orderID = :selectedOrderID;
+DELETE FROM Orders WHERE orderID = %s;
 
 -- For delete Products buttons
-DELETE FROM Products WHERE productID = :selectedProductID;
+DELETE FROM Products WHERE productID = %s;
 
 -- for delete Stores buttons
-DELETE FROM Stores WHERE storeID = :selectedStoreID;
+DELETE FROM Stores WHERE storeID = %s;
